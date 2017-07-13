ffa112f6cf16a9112005829dd86af5c8cc64cd6e
Resilient Counsellor <resilientcounsellor@gmail.com>
2017-07-13 14:04:18 +0000

    initial commit

---
+++ resil person.py
@@ -0,0 +1,14 @@
+def get_age():
+	age = (int)(raw_input("Whats your age?"))
+	return age
+
+def get_name():
+	return raw_input("whats your name?"
+
+age = get_age()
+name = get_name()
+print "hey, " + name + ", you are + "years old, dude!"
+
+def calculate(op1, op2, f):
+   if f == "add":
+	returny
